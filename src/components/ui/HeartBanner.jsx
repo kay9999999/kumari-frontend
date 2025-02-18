23270440015bcec9134cf0956813a3619189577c
bikramsk <138878880+bikramsk@users.noreply.github.com>
2025-02-18 13:59:01 +0530

    Update HeartBanner.jsx

--- src/components/ui/HeartBanner.jsx
+++ src/components/ui/HeartBanner.jsx
@@ -1,49 +1,49 @@
 import React from "react";
 import Link from "next/link";
 import ReadMoreButton from "./ui/readmorebutton";
-import { getStrapiURL } from "@/lib/utils";
+
 
 const HeartBanner = () => {
   return (
     
-
-      <div className="relative bg-white my-[20px] flex flex-col lg:flex-row ">
+      <div className="relative bg-white pt-40 max-lg:pt-0 flex flex-col lg:flex-row ">
 
         {/* Content Container */}
-        <div className="content-data w-full max-w-[1700px] z-10 space-y-4 px-4 order-1 lg:relative lg:z-10 mt-24">
+       
+        <div className=" w-full max-w-[1700px] z-10 space-y-4 px-4 order-1 lg:order-1 lg:absolute lg:z-10 lg:mt-0 lg:w-1/2 lg:pl-8 lg:top-0 xl:top-0 lg:left-0 lg:flex lg:flex-col lg:justify-start lg:items-start ">
           <Link
             href="/our-collections"
-            className="min-h-[50px] md:min-h-[350px] z-10 flex flex-col justify-center items-start py-[60px] px-5 relative no-underline"
+            className="hearts-content min-h-[50px]  xl:top-20 2xl:top-20 z-10 flex flex-col justify-start items-start py-[10px] px-5 relative no-underline lg:pt-0 xl:mt-60 " 
           >
-            <h2 className="text-[40px] font-primary max-w-[340px] tracking-tight font-light leading-[1.2] mb-3">
+            <h2 className="text-[38px] font-primary max-w-[340px] tracking-tight font-light leading-[1.2] mb-3 lg:mb-0 z-20">
               ALL MOODS <i>of</i> KUMARI
             </h2>
             <ReadMoreButton link="/our-collections" label="shop all collections" className="px-4 py-2 text-sm hover:scale-x-110 hover:translate-x-2" />
           </Link>
         </div>
-
+        
         {/* Image Wrapper */}
-        <div className="w-full order-2 lg:absolute lg:top-0 lg:right-0 lg:bottom-0 lg:left-0 ">
+        <div className="w-full order-2 lg:order-2 lg:w-full lg:relative lg:top-0 lg:bottom-0 lg:right-0 ">
           <Link href="/our-collections" className="z-0 w-full flex justify-end">
             <picture>
-              
               <source
-                srcSet={`${getStrapiURL()}${productData.data.image[0].url}`}
+                srcSet="/images/product/Kumari_All_collection_banner_Mobile_1.jpg"
                 media="(max-width: 479px)"
               />
               <source
-                srcSet={`${getStrapiURL()}${productData.data.image[1].url}`}
+                srcSet="/images/product/Kumari_All_collection_banner_Tablet.jpg"
                 media="(max-width: 1024px)"
               />
               <img
-                src={`${getStrapiURL()}${productData.data.image[2].url}`}
+                src="/images/product/Kumari_All_collection_banner_Desktop_1.jpg"
                 alt="Kumari All Collection Banner"
-                className="object-cover w-[1200px] h-auto max-md:w-full"
+                className="object-cover w-[1200px] h-auto"
               />
             </picture>
           </Link>
         </div>
       </div>
+
   );
 };
 
